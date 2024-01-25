a1ce3ab2c935db2bbe2bc794a9c60c66f5bf4f60
abdul <wahab5903@gmail.com>
2024-01-25 18:30:23 +0500

    requirement file added

--- test.py
+++ test.py
@@ -8,24 +8,12 @@ from pytest_bdd import *
 
 
 
-def test_visit_youtube(page,browser_type):
-    browser=browser_type.launch(headless=True,slow_mo=6000)
-    page=browser.new_page()
-    page.goto("https://youtube.com")
-    browser.close()
-    breakpoint()
 
 def test_has_title(page,browser_type):
     browser = browser_type.launch(headless=True, slow_mo=6000)
     page = browser.new_page()
     page.goto("https://playwright.dev/")
-    page.get_by_placeholder("Email, phone, or Skype").click()
-    page.get_by_placeholder("Email, phone, or Skype").fill("abdul.wahab@spglobal.com")
-    page.get_by_role("button", name="Next").click()
-    page.get_by_placeholder("Password").click()
-    page.get_by_placeholder("Password").fill("$ecurity@964@")
-    page.get_by_role("button", name="Sign in").click()
-    page.wait_for_timeout(timeout=5000)
+
 
     page.title()
 
