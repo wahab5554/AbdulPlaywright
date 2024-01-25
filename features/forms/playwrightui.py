2fd10fdeea8746766245dce30f08b6786ed990e4
abdul <wahab5903@gmail.com>
2024-01-25 18:35:19 +0500

    requirement file added

--- features/forms/playwrightui.py
+++ features/forms/playwrightui.py
@@ -9,7 +9,7 @@ class PlaywrightUI:
 
     def navigate_url(self, url):
         with sync_playwright() as p:
-            browser = p[self.browser_type].launch(headless=False, slow_mo=6000)
+            browser = p[self.browser_type].launch(headless=True, slow_mo=6000)
             self.page = browser.new_page()
             self.page.goto(url)
 
